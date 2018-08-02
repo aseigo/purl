4730c138ca92da0dd04c671bf9d075342ca846fe
Aaron Seigo <aseigo@kde.org>
2018-08-02 22:32:54 +0200

    shorter, more declarative

--- examples/example.ex
+++ examples/example.ex
@@ -8,35 +8,33 @@ defmodule Purl.Example do
   def init(state), do: state
 
   proto handshake(accepting: :hello, timeout: 1000) do
-    on :start_v1, switch_proto: v1_auth
+    on :start_v1,
+      switch_proto: v1_auth
   end
 
   proto v1_auth(accepting: :apikey, timeout: 1000) do
-    on :no_such_api_key do
-        response(:error, <<"no such key">>)
-        :terminate
-    end
+    on :no_such_api_key,
+      reponse: [:error, <<"no such key">>],
+      terminate: true
 
-    on :ready, switch_proto: :v1_init_job
+    on :ready,
+      switch_proto: :v1_init_job
   end
 
   proto v1_init_job(accepting: :job_id, timeout: 1000) do
-    on :no_such_job_id do
-        response(:error, <<"no such job">>)
-        :terminate
-    end
+    on :no_such_job_id,
+      response: [:error, <<"no such job">>],
+      terminate: true
 
-    on :ready, switch_proto: :v1_main
+    on :ready,
+      switch_proto: :v1_main
   end
 
-  proto v1_main(processing: run_job, accepting: [:terminate, :client_info_json, :client_info_etf]) do
-    on :job_set, do: :run_job
+  proto v1_main(processing: :run_job, accepting: [:terminate, :client_info_json, :client_info_etf]) do
+    on :job_done,
+      terminate: true
 
-    on :job_done, do: :terminate
-
-    on :job_msg do
-      response(:info, data.message)
-      :run_job
-    end
+    on :job_msg,
+      response: [:info, data.message]
   end
 end
