00ba74f011626233409415d3e2fdeb34b34f2848
Aaron Seigo <aseigo@kde.org>
2018-08-02 21:38:46 +0200

    more variations on the theme, "respond", and timeouts to proto

--- examples/example.ex
+++ examples/example.ex
@@ -5,27 +5,29 @@ defmodule Purl.Example do
 
   accept init,
     type: :fixed_message,
-    match: "HH",
+    message: "HH",
     handler: fn -> :start_v1 end
 
   accept apikey,
     type: :fixed_size_message,
     size: 36,
-    timeout: 1000,
     handler: :verify_key
 
   accept job_id,
     type: :terminated_buffer,
     end_token: <<0>>,
-    timeout: 1000,
     handler: :verify_job_id
 
   accept terminate,
+    type: :fixed_message,
+    message: "99",
+    handler: fn -> :terminate end
+
+  accept ping,
     type: :tagged_message,
-    tag: "99",
+    tag: "ping",
     end_token: <<0>>,
-    timeout: 1000,
-    handler: :abort
+    response: :pong
 
   accept client_info,
     type: :tagged_varlength_message,
@@ -38,44 +40,49 @@ defmodule Purl.Example do
     timeout: :infinite,
     handler: :exec
 
-  response error,
+  respond error,
     type: :tagged_message,
     tag: "11",
     end_token: "\n"
 
-  response info,
+  respond info,
     type: :tagged_varlength_message,
     tag: "20"
 
-  proto handshake(accepting: :init) do
+  respond pong,
+    type: :fixed_message
+    message: "pong" <> <<0>>
+    
+
+  proto handshake(accepting: :init, timeout: 1000) do
     on :start_v1, switch_proto: v1_auth
   end
 
-  proto v1_auth(accepting: :apikey) do
+  proto v1_auth(accepting: :apikey, timeout: 1000) do
     on :no_such_api_key do
-        respond(:error, <<"no such key">>)
+        response(:error, <<"no such key">>)
         :terminate
     end
 
     on :ready do, switch_proto: :v1_init_job
   end
 
-  proto v1_init_job(accepting: :job_id) do
+  proto v1_init_job(accepting: :job_id, timeout: 1000) do
     on :no_such_job_id do
-        respond(:error, <<"no such job">>)
+        response(:error, <<"no such job">>)
         :terminate
     end
 
     on :ready do, switch_proto: :v1_main
   end
 
-  proto v1_main(processing: run_job, accepting: :client_info) do
+  proto v1_main(processing: run_job, accepting: [:terminate, :client_info]) do
     on :job_set, do: :run_job
 
     on :job_done, do: :terminate
 
     on :job_msg do
-      respond(:info, data.message)
+      response(:info, data.message)
       :run_job
     end
   end
